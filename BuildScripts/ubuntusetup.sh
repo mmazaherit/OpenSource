e2bab0cc16f9d358159c36c2e2b1ef905781f91c
mehdi.mazaheri <m.mazaheri.t@gmail.com>
2017-08-20 20:18:24 -0400

    update

--- BuildScripts/ubuntusetup.sh
+++ BuildScripts/ubuntusetup.sh
@@ -8,8 +8,16 @@ sudo add-apt-repository ppa:xorg-edgers/ppa -y
 sudo add-apt-repository ppa:plushuang-tw/uget-stable -y
 sudo apt-get update
 
+#install tools
 sudo apt-get install g++ -y
+sudo apt-get install aptitude -y
+sudo apt-get install rpm synaptic fluxgui uget -y
+sudo apt-get install git subversion yum -y
+sudo apt-get install rar unrar -y
+sudo apt-get install vlc -y
+export VDPAU_DRIVER=vdpau
 mkdir ~/thirdparty
+
 
 #install cmake-gui from source, needs qt
 #sudo apt-get purge cmake -y
@@ -22,14 +30,19 @@ cd ~/thirdparty/cmake-3.9.0
 make -j4
 sudo make install
 
-#install python3 from source
-sudo apt-get install -y libbz2-dev zlib1g-dev
-wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tgz -P ~/thirdparty
-cd ~/thirdparty
-tar xzf Python-3.6.2.tgz
-cd Python-3.6.2
-./configure --enable-optimizations --with-ensurepip=install
-sudo make install
+#install python from source
+#sudo apt-get install -y libbz2-dev zlib1g-dev
+#sudo apt-get install -y libexpat1-dev libssl-dev libncurses5-dev  liblzma-dev  libsqlite3-dev libffi-dev tcl-dev #libgdbm-dev libreadline-dev tk tk-dev
+#wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tgz -P ~/thirdparty
+#cd ~/thirdparty & tar xzf Python-3.6.2.tgz
+#cd ~/thirdparty/Python-3.6.2
+#./configure --enable-optimizations --with-ensurepip=install 
+#make 
+#make install
+
+#install anaconda, pyton + scientific packages
+wget https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh -P ~/Downloads
+bash ~/Downloads/Anaconda3-4.4.0-Linux-x86_64.sh
 
 
 #install qt, cmake gui needs it
@@ -48,22 +61,18 @@ cmake ..
 make 
 sudo make install
 
-#install tools
-sudo apt-get install rpm synaptic fluxgui vlc uget -y
-sudo apt-get install git subversion yum -y
-sudo apt-get install rar unrar -y
+
 
 #install rabbitvcs
 sudo apt-get install rabbitvcs-nautilus3 rabbitvcs-nautilus rabbitvcs-thunar rabbitvcs-gedit rabbitvcs-cli -y
 chown -R $USER:$USER ~/.config/rabbitvcs
 
-#install ROS indigo
-sudo apt-get install ros-indigo-desktop-full -y
-echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
-source /opt/ros/indigo/setup.bash
-sudo rosdep init
-rosdep update
-sudo apt-get install -y python-rosinstall python-wstool python-rosdep ninja-build
+#install ROS 
+sudo apt-get install ros-kinetic-desktop-full -y
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source /opt/ros/kinetic/setup.bash
+sudo rosdep init && rosdep update
+sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential ninja-build -y
 
 
 #install standard ros packages, build with catkin_make
@@ -73,7 +82,7 @@ git clone https://github.com/swri-robotics/gps_umd.git
 git clone https://github.com/ros-geographic-info/geographic_info.git 
 cd ~/catkin_ws/
 rosdep update
-rosdep install --from-paths src --ignore-src --rosdistro=indigo -y 
+rosdep install --from-paths src --ignore-src --rosdistro=kinetic -y 
 catkin_make install
 
 #non-standard packages install
@@ -83,7 +92,7 @@ wstool init src
 wstool merge -t src https://raw.githubusercontent.com/googlecartographer/cartographer_ros/master/cartographer_ros.rosinstall
 wstool update -t src
 rosdep update
-rosdep install --from-paths src --ignore-src --rosdistro=indigo -y
+rosdep install --from-paths src --ignore-src --rosdistro=kinetic -y
 catkin_make_isolated --install --use-ninja
 
 #install virtualbox, secure boot must be disabled from bios
@@ -94,8 +103,28 @@ wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key a
 sudo apt-get update
 sudo apt-get install virtualbox-5.1 -y
 
+#install samba
+#sudo aptitude install samba samba-common system-config-samba python-glade2 gksu
+#sudo cp /usr/share/samba/smb.conf /etc/samba/smb.conf
+#sudo dpkg --configure -a
+#sudo smbpasswd -a $USER
+#sudo cp /etc/samba/smb.conf ~
 
+#install chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P ~/Downloads
+sudo dpkg -i --force-all ~/Downloads/google-chrome*.deb
+#install pycharm
+rm -r ~/.PyCharm*/
+wget https://download.jetbrains.com/python/pycharm-community-2017.2.1.tar.gz -P ~/Downloads
+cd ~/Downloads && tar xzf pycharm-community-2017.2.1.tar.gz
+cd  ~/Downloads/pycharm-community-2017.2.1/bin
+#install onedrive
+cd ~/thirdparty
+git clone https://github.com/xybu92/onedrive-d.git
+cd onedrive-d
+./install-sh -y
+onedrive-pref
 
-
-
+#add the following to startup
+#onedrive-d start
 
